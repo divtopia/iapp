ab959c6344693ce5bd6c79861183b3dcd476bf6f
JP Adams <divtopia@gmail.com>
2010-11-06 17:32:45 -0400

    Coded the initial layout but no functionality as of yet

---
+++ assets/js/stronglifters.js
@@ -0,0 +1,16 @@
+jqt = new $.jQTouch({
+	icon: 'jqtouch.png',
+	statusBar: 'black-translucent',
+	preloadImages: [
+		'/assets/js/jqtouch/themes/jqt/img/chevron_white.png',
+		'/assets/js/jqtouch/themes/jqt/img/bg_row_select.gif',
+		'/assets/js/jqtouch/themes/jqt/img/back_button_clicked.png',
+		'/assets/js/jqtouch/themes/jqt/img/button_clicked.png'
+	]
+});
+
+$(function(){
+	$("#Options .back").click(function(){
+		
+	})
+});
