01b223e7bb30e5d4933d523149c87c92be3b096e
JP Adams <divtopia@gmail.com>
2010-11-07 02:42:32 -0500

    some changes to the index file

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,22 +3,22 @@ var jQT = new $.jQTouch({
    addGlossToIcon: true,
    startupScreen: 'jqt_startup.png',
    statusBar: 'black',
-   preloadImages: [
-	assets/js/jqtouch/themes/jqt/img/back_button.png,
-	assets/js/jqtouch/themes/jqt/img/chevron_circle.png,
-	assets/js/jqtouch/themes/jqt/img/toggle.png,
-	assets/js/jqtouch/themes/jqt/img/back_button_clicked.png,
-	assets/js/jqtouch/themes/jqt/img/grayButton.png,
-	assets/js/jqtouch/themes/jqt/img/toggleOn.png,
-	assets/js/jqtouch/themes/jqt/img/button.png,
-	assets/js/jqtouch/themes/jqt/img/loading.gif,
-	assets/js/jqtouch/themes/jqt/img/toolbar.png,
-	assets/js/jqtouch/themes/jqt/img/button_clicked.png,
-	assets/js/jqtouch/themes/jqt/img/on_off.png,
-	assets/js/jqtouch/themes/jqt/img/whiteButton.png,
-	assets/js/jqtouch/themes/jqt/img/chevron.png,
-	assets/js/jqtouch/themes/jqt/img/rowhead.png
-   ]
+   preloadImages: [	
+	'assets/js/jqtouch/themes/jqt/img/back_button.png',
+	'assets/js/jqtouch/themes/jqt/img/chevron_circle.png',
+	'assets/js/jqtouch/themes/jqt/img/toggle.png',
+	'assets/js/jqtouch/themes/jqt/img/back_button_clicked.png',
+	'assets/js/jqtouch/themes/jqt/img/grayButton.png',
+	'assets/js/jqtouch/themes/jqt/img/toggleOn.png',
+	'assets/js/jqtouch/themes/jqt/img/button.png',
+	'assets/js/jqtouch/themes/jqt/img/loading.gif',
+	'assets/js/jqtouch/themes/jqt/img/toolbar.png',
+	'assets/js/jqtouch/themes/jqt/img/button_clicked.png',
+	'assets/js/jqtouch/themes/jqt/img/on_off.png',
+	'assets/js/jqtouch/themes/jqt/img/whiteButton.png',
+	'assets/js/jqtouch/themes/jqt/img/chevron.png',
+	'assets/js/jqtouch/themes/jqt/img/rowhead.png'
+	]
 });
 
 $(function(){
