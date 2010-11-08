57a6d1b806b1896fa7250282163ab7f265fb2056
JP Adams <divtopia@gmail.com>
2010-11-08 01:54:23 -0500

    checking in start to js functionality

--- assets/js/common.js
+++ assets/js/common.js
@@ -22,5 +22,77 @@ var jQT = new $.jQTouch({
 });
 
 $(function(){
-
+	DataKeeping();
 });
+
+function DataKeeping(){
+	// init a new lawnchair db
+	var workouts = new Lawnchair('workouts');
+	if(workouts) {
+		workouts.nuke();
+	}
+
+	var currKey = '';
+
+	// new date
+	var d = new Date();
+	var currday = d.getDate()
+	var currmon = d.getMonth();
+	var curryr = d.getFullYear();
+	var currhr = d.getHours() >= 10 ? d.getHours(): 0 + '' + d.getHours();
+	var currmin =d.getMinutes() >= 10 ? d.getMinutes() : 0 + '' + d.getMinutes();
+	// Get a human readable date
+	var currdate = curryr + '' + ((currmon >= 10) ? currmon : (0 + ''+currmon)) +''+ ((currday>=10) ? currday : (0 + ''+currday)) +''+ currhr + '' + currmin; 
+	console.log(currdate);
+
+	var currStat = {
+		key: currdate,
+		usr: {
+			name:'JP',
+			email:'jugga23@gmail.com'
+		},
+		workoutstats: {
+			exerciseset:1,
+			weights:{
+				squats:45,
+				bench:45,
+				reverse:0
+			},
+			succeededall: 1
+		}
+	};
+	// save currstat
+	workouts.save(currStat);
+	// return all works saved as an object
+	workouts.all('console.log(r)');
+	var username = GetUserName(workouts, currdate);
+	console.log(username);
+	// return bench weight -- '45'
+	workouts.find('r.workoutstats.date == "7102010"', 'console.log(r.workoutstats.weights.bench)');
+	// store key in a variable
+	workouts.find('r.workoutstats.date == "7102010"', 'currKey = r.key');
+	// use key variable -- 'FB54E444-7156-4CDF-A71C-ABD9A8790A2E'
+	console.log(currKey);
+	
+	
+}
+
+var NewWorkout = function( currentdate ){
+	var previousdate = GetPrevDate();
+	return {key: currentdate};
+}
+
+var GetPrevDate = function( workout ) {
+	// get key of the previous object
+}
+
+var GetUserName = function ( workouts, currdate ) {
+	// returns name -- 'JP'
+	var username = '';
+	workouts.find(function(r) {
+		if(r.key == currdate){
+			username = r.usr.name;
+		}
+	});
+	return username;
+}
